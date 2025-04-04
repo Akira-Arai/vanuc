464fd49fa469941c0841d59421f6e22d583294b6
Akira Arai <95276601+Akira-Arai@users.noreply.github.com>
2025-04-04 17:47:39 +0900

    Update vanuc_pre.m
    Resolution of serious errors when reading TPM files

--- vanuc_pre.m
+++ vanuc_pre.m
@@ -350,7 +350,7 @@ pause(0.1);
 matlabbatch{1}.spm.spatial.normalise.est.subj.vol = {'coT1.nii,1'};
 matlabbatch{1}.spm.spatial.normalise.est.eoptions.biasreg = 0.0001;
 matlabbatch{1}.spm.spatial.normalise.est.eoptions.biasfwhm = 60;
-matlabbatch{1}.spm.spatial.normalise.est.eoptions.tpm = {strcat(PathV,'\standard\TPM.nii')};
+matlabbatch{1}.spm.spatial.normalise.est.eoptions.tpm = {PathTPM};
 matlabbatch{1}.spm.spatial.normalise.est.eoptions.affreg = 'mni';
 matlabbatch{1}.spm.spatial.normalise.est.eoptions.reg = [0 0.001 0.5 0.05 0.2];
 matlabbatch{1}.spm.spatial.normalise.est.eoptions.fwhm = 0;
